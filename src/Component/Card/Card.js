7e9e5f20fc5314828b81f491c906e6af6e2184f8
mmozammel99 <mhfaryanemon@gmail.com>
2022-09-29 06:15:40 +0600

    side bar add

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -2,8 +2,9 @@ import { faClock, faListCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-const Card = (props) => {
-    const { name, img, description, step, time } = props.exercise;
+const Card = ({ exercise, addToList }) => {
+    const { name, img, description, step, time } = exercise;
+
     return (
         <div className="card card-compact w-70  bg-base-100 shadow-xl relative rounded-xl pb-16">
             <img className='rounded-2xl p-2' src={img} alt="" />
@@ -14,7 +15,7 @@ const Card = (props) => {
                 <p className='font-medium'> <FontAwesomeIcon icon={faClock}></FontAwesomeIcon> Time required : {time}s</p>
             </div>
             <button className='w-[95%] border text-white bg-green-600 font-bold 
-                rounded-xl hover:text-green-600 hover:bg-white hover:border-green-600 absolute bottom-0 p-2 m-2'>Add to list</button>
+                rounded-xl hover:text-green-600 hover:bg-white hover:border-green-600 absolute bottom-0 p-2 m-2' onClick={() => addToList(exercise)}>Add to list</button>
 
 
 
--- src/Component/Exercise/Exercise.js
+++ src/Component/Exercise/Exercise.js
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from 'react';
 import Card from '../Card/Card';
 
 const Exercise = () => {
-    const [exercises, setExercise] = useState([])
+     const [exercises, setExercise] = useState([]);
+     const [list, setList] = useState([]);
+
     useEffect(() => {
         fetch('exercises.json')
             .then(res => res.json())
             .then(data => setExercise(data))
     }, [])
+
+    const addToList = (exercises) =>{
+        console.log(exercises);
+        const newList = [...list, exercises]
+setList(newList)
+    }
     return (
 
         <div className='grid grid-cols-3 gap-5 mx-12 mt-2'>
@@ -15,6 +23,8 @@ const Exercise = () => {
                 exercises.map(exercise => <Card
                     key={exercise.id}
                     exercise={exercise}
+ addToList ={addToList} 
+
                 ></Card>)
             }
 
