18e803267c60458e554f410ea92d6054334d44ff
mmozammel99 <mhfaryanemon@gmail.com>
2022-09-29 18:36:19 +0600

    blog3

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -37,7 +37,54 @@ const Blog = () => {
                     </div>
                 </div>
             </div>
-            
+            <div className="card w-[90%] bg-base-100 shadow-xl rounded-xl m-auto lg:m-16 p-10 lg:mr-32 text-justify">
+                <div className="card-body">
+                    <h2 className="card-title text-3xl font-bold mb-2">6 use cases of the useEffect ReactJS hook</h2>
+                    <p>Whenever we need to make use of side effects in our application, useEffect is the way to go. This hook doesn't present many complications, except for non-primitive data types, due to how JavaScript handles them.
+                        <br />
+                        According to the official documentation, effects run after every completed render, but you can choose to fire them only when certain values have changed. This hook uses an array of "dependencies": variables or states that useEffect listen to for changes. When their values change, the main body of the useEffect hook is executed.
+                        <br />
+                        The return statement of this hook is used to clean methods that are already running, such as timers. The first time this hook is called, its main body is the one that is going to be evaluated first. All other subsequent times the hook is called, the return statement will be evaluated first, and, after that, the hook's main body. This behaviour is especially useful for cleaning code that is already running before run it again, which enable us to prevent memory leaks.
+                        <br />
+                        There is an interesting behaviour with this hook when we use non-primitive JavaScript data types as dependencies (e.g., arrays, objects, functions). With primitive values, such as numbers and strings, we can define a variable from another variable, and they will be the same, But with non-primitive values, such as objects, this behaviour is not the same.So we need to be very careful when using objects as dependencies, because even though they may look like unaltered data, they may not be so. Instead of using objects, we may want to use their properties as dependencies.
+                        Now, let's take a look at some use cases for this hook.
+                        <ol><li>1.Running once on mount: fetch API data</li>
+                            <li>2.Running on state change: validating input field</li>
+                            <li>3.Running on state change: live filtering</li>
+                            <li>4.Running on state change: trigger animation on new array value</li>
+                            <li>5.Running on props change: update paragraph list on fetched API data update</li>
+                            <li>6.Running on props change: updating fetched API data to get BTC updated price</li>
+                        </ol>
+                        <br />
+                        <span className='font-semibold'>Running once on mount: fetch API data</span>
+                        <br />
+                        When we want to perform an action once, especially when the app loads or mounts, we can use useEffect to do it. In this case, we are triggering a fetch() GET request when the app is mounted, using an empty array as useEffect dependency.
+                        <br />
+                        <span className='font-semibold'>Running on state change: validating input field</span>
+                        <br />
+                        Validating an input while it's receiving characters is another great application for useEffect. Whilst the input is being stored in a state using useState, the validation takes place every time the input changes, giving immediate feedback to the user.
+                        <br />
+                        We could add a setTimeout() function to check the input field after some time, to delay the checking on each user keystroke, and we would need to clear that timer by using the clearTimeout() function in the return statement of the useEffect hook. A similar example of this is implemented in the useEffect animation trigger, further ahead.
+                        <br />
+                        <span className='font-semibold'>Running on state change: live filtering</span>
+                        <br />
+                        We can use useEffect to filter an array "on the fly" by typing letters into an input element. To do so, we will need to use a state to save the input, and a filter implementation inside the useEffect that will be triggered when the input changes, thanks to useEffect dependencies.
+                        <br />
+                        <span className='font-semibold'>Running on state change: trigger animation on new array value</span>
+                        <br />
+                        We can use the useEffect hook to trigger an animation on a shopping cart as a side effect of adding a new product to it. In this case, we'll need a state to handle the cart items, and another state to handle the animation trigger.
+                        <br />
+                        As we are using a timer inside the useEffect, It is a good practice to clear it before it gets set again by using the return statement of the useEffect, which gets executed before the main body of the useEffect hook gets evaluated (except for the first render).
+                        <br />
+                        <span className='font-semibold'>Running on props change: update paragraph list on fetched API data update</span>
+                        <br />
+                        In this use case, we want to trigger a state update due to an updated fetch() call. We are sending the fetched data to a child component, and whenever that data is changed, the child component re-process it.
+                        <br />
+                        <span className='font-semibold'>Running on props change: updating fetched API data to get updated BTC price</span>
+                        <br />
+                        In this example, useEffect is used to fetch new data from an API every 3 seconds. The child component useEffect receives the time as dependency and every time that dependency changes, a new fetch() is triggered. This way, we can have an updated BTC exchange rate in our app.</p>
+                </div>
+            </div>
 
         </div>
     );
